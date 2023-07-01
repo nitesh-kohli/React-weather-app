60d58f1d217d4023cbcbbf82489d0cace293cd38
nitesh <niteshkoli015@gmail.com>
2023-07-01 18:10:17 +0530

    second commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // axios is a library you can call APi with the help of it
 
 import axios from 'axios';
-
+const API_KEY = '48fa9838598b24c40946dc33d60f44c6';
 const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 
@@ -9,7 +9,7 @@ const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 const getWeather = async (city, country) => {
     try {
-        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
         return response.data;
     } catch (error) {
         console.log('Error while calling the api ', error.message);
